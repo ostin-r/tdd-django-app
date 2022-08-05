a6494d0ed708e7e8eaefb2c797acb05ea92cbc2b
ostin-r <austinjrichards@outlook.com>
2022-08-05 12:48:35 -0600

    Create helper for (functional) testing values are being added to the table

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')  # note this is finding multiple elements in table
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000/')
         self.assertIn('To-Do', self.browser.title)
@@ -29,9 +34,8 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')  # note this is finding multiple elements in table
-        self.assertIn('1: Buy soup', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy soup')
+        self.check_for_row_in_list_table('2: Cook soup')
         
         self.fail('Finish the test!')
 
