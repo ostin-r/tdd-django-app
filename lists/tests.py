ff7b2fb90a783c98b74ab3f8cf517f0db89190cf
ostin-r <austinjrichards@outlook.com>
2022-08-05 14:37:10 -0600

    Initialize django ORM with a table that can hold users text for list items

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -15,3 +17,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'new list item'})
         self.assertIn('new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_write_and_read_list_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second item')
